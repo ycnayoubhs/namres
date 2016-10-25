c2843b9f38c954af44590678078f5fa86d7e9e35
Jacksing <hijackyeah@163.com>
2016-10-25 16:19:56 +0800

    Mod page layout and document authority

--- mixed/urls.py
+++ mixed/urls.py
@@ -10,7 +10,6 @@ urlpatterns += patterns(
     'mixed.views',
     url(r'^a/create/$', 'document_create', name='create'),
     url(r'^a/edit/(?P<slug>.*)/$', 'document_edit', name='edit'),
-    # url(r'^a/save/$', 'document_save', name='save'),
     url(r'^a/delete/(?P<slug>.*)/$', 'document_delete', name='delete'),
     url(r'^$', 'document_list', name='list'),
     url(r'^(?P<slug>.*)/$', 'document_view', name='view'),
--- mixed/views.py
+++ mixed/views.py
@@ -12,6 +12,7 @@ from .server_list_converter import convert_server_list
 from .custom_converter import convert_customizable
 
 
+@login_required
 def document_create(request):
     if request.method == 'GET':
         content = {
@@ -34,20 +35,35 @@ def document_create(request):
             content.update(csrf(request))
             return render_to_response('doc_editor.html', content)
 
+        form.instance.user = request.user
         form.save()
 
         return redirect(reverse('list'))
 
 
-@login_required
-def document_edit(request, slug=None):
-    if request.method == 'GET':
+def doc_owner_required(function):
+    def _doc_owner_required(request, slug):
         try:
             document = Document.objects.get(slug=slug, is_deleted=False)
-            form = DocumentForm(instance=document)
         except Document.DoesNotExist:
             message = 'Document %s not exists or maybe deleted.' % slug
             return redirect('%s?%s' % (reverse('list'), urlencode({'message': message})))
+        
+        if document.user and request.user.id != document.user.id:
+            message = 'You have no authority to do this action to <%s>.' % document.name
+            return redirect('%s?%s' % (reverse('list'), urlencode({'message': message.encode('utf-8')})))
+
+        request.document = document
+        return function(request, slug)
+    return _doc_owner_required
+
+
+@login_required
+@doc_owner_required
+def document_edit(request, slug):
+    if request.method == 'GET':
+        document = request.document
+        form = DocumentForm(instance=document)
 
         content = {'form': form, 'title': document.name, 'username': request.user.username,}
         content.update(csrf(request))
@@ -55,7 +71,7 @@ def document_edit(request, slug=None):
         return render_to_response('doc_editor.html', content)
 
     elif request.method == 'POST':
-        document = get_object_or_404(Document, slug=slug)
+        document = request.document
         form = DocumentForm(request.POST, instance=document)
 
         if not form.is_valid():
@@ -72,10 +88,12 @@ def document_edit(request, slug=None):
         return redirect(reverse('list'))
 
 
+@login_required
+@doc_owner_required
 def document_delete(request, slug):
     message = None
     try:
-        document = Document.objects.get(slug=slug, is_deleted=False)
+        document = request.document
         if document.converter != 'S':
             document.is_deleted = True
             document.save()
